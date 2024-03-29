eb6683583e5a3e193fe799bd8e6e1ed505d4b433
armandi10 <armand.xhafa@hotmail.com>
2024-03-29 22:13:09 +0100

    adding new subnet

--- create_aws_vpc.tf
+++ create_aws_vpc.tf
@@ -5,4 +5,18 @@ resource "aws_vpc" "main" {
 # This is a comment
 # Adding new resources
 
+<<<<<<< HEAD
 # this should be a conflict
+=======
+
+
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+>>>>>>> f729e37 (adding new subnet)
